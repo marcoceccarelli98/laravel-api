8d17a258e44bfc7a5e062875726e058a77b8e3bf
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-30 15:48:53 +0200

    [Milestone 2]

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ProjectController;
+
 use App\Http\Controllers\Guest\PageController;
 
 use App\Http\Controllers\ProfileController;
@@ -48,5 +49,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
